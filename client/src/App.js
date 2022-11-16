d1458fdc683bc06db54491b2acf34b0eb2399eca
cvkulkarni2810 <cvkulkarni2810@gmail.com>
2022-11-16 10:11:24 +0530

    material table

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,20 @@ import OngoingAuctionTable from './commission_agent/ongoing_auction_table';
 import PreviousLotTable from './commission_agent/previous_lot_table';
 import LotTable from './commission_agent/table_component';
 import {Routes,Route ,BrowserRouter as Router} from 'react-router-dom'; 
+import MuiLotTable from './commission_agent/muiLotTable';
+import LotDetailTable from './table';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <LotPendCommTable/>
+      {/* <LotPendCommTable/>
       <OngoingAuctionTable/>
       <PreviousLotTable/>
       <LotTable/>
+      <MuiLotTable/> */}
+      <LotDetailTable/>
+      
     </div>  
 
   );
